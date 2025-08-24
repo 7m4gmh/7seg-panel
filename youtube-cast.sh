4f22cf3b4eec5ef7f8c9296f0b301ac4411d1123
Hijiri Umemoto <hijiri@umemoto.org>
2025-08-25 02:39:42 +0900

    youtube streaming

--- youtube-cast.sh
+++ youtube-cast.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
 # =======================================================
-# ▼▼▼ 設定する項目 ▼▼▼
-# =======================================================
 
-# 1. YouTubeのストリームキーをここに設定してください
-STREAMKEY="auk9-d9vb-cp3w-x2rz-by5b"
+PASS_FILE="./stream_key.conf"
 
-# 2. カメラのデバイスパス
+if [ ! -f "$PASS_FILE" ]; then
+    echo "エラー: パスワードファイルが見つかりません: $PASS_FILE"
+    exit 1
+fi
+
+read -r STREAMKEY < "$PASS_FILE"
+
+if [ -z "$STREAMKEY" ]; then
+    echo "エラー: パスワードファイルが空か、読み込みに失敗しました。"
+    exit 1
+fi
 VIDEO_DEVICE="/dev/video1"
 
 # =======================================================
@@ -23,11 +30,22 @@ fi
 echo "INFO: Using audio monitor source: ${MONITOR}"
 
 # CPUエンコーダ(libx264)を指定して実行
+#  Hardware encoder  h264_rkmpp
 # thread_queue_sizeを追加して安定性を向上
+#/usr/local/bin/ffmpeg -hide_banner -loglevel info \
+# -thread_queue_size 2048 -f v4l2 -input_format mjpeg -framerate 24 -video_size  1280x720 -i "${VIDEO_DEVICE}" \
+# -thread_queue_size 2048 -f pulse -i "${MONITOR}" \
+# -c:v h264_rkmpp  -b:v 2500k -maxrate 2500k -bufsize 5000k -g 60 \
+# -c:a aac -b:a 128k -ar 48000 -ac 2 -r 24 \
+# -f flv "rtmp://a.rtmp.youtube.com/live2/${STREAMKEY}"
+
+VIDEO_DEVICE="/dev/video1" # デバイスを video3 に変更
+
 /usr/local/bin/ffmpeg -hide_banner -loglevel info \
- -thread_queue_size 1024 -f v4l2 -input_format mjpeg -framerate 30 -video_size 1280x720 -i "${VIDEO_DEVICE}" \
- -thread_queue_size 1024 -f pulse -i "${MONITOR}" \
- -c:v libx264 -preset veryfast -b:v 4000k -maxrate 4000k -bufsize 8000k -g 60 \
- -c:a aac -b:a 128k -ar 44100 -ac 2 \
+ -f v4l2 -input_format yuyv422 -framerate 15 -video_size 800x600 -i "${VIDEO_DEVICE}" \
+ -thread_queue_size 2048 -f pulse -i "${MONITOR}" \
+ -c:v h264_rkmpp -b:v 1500k -maxrate 1500k -bufsize 3000k -g 30 \
+ -c:a aac -b:a 128k -ar 48000 -ac 2 \
+ -r 15 \
  -f flv "rtmp://a.rtmp.youtube.com/live2/${STREAMKEY}"
 
