618a0fa617edcfb4108a7d62832457f6df3f55d1
Hijiri Umemoto <hijiri@umemoto.org>
2025-08-25 08:58:03 +0900

    camera control

---
+++ camera_set.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# フォーカスをオフにして固定
+v4l2-ctl -d /dev/video1 -c focus_automatic_continuous=0
+v4l2-ctl -d /dev/video1 -c focus_absolute=470
+
+# 露出をオフにして固定
+v4l2-ctl -d /dev/video1 -c auto_exposure=1
+v4l2-ctl -d /dev/video1 -c exposure_time_absolute=157
+
+v4l2-ctl -d /dev/video1 -c white_balance_automatic=0
+
+
